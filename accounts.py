701576919a144b8f4dd14177e87d10451be9424e
SlavaKravtsov2025 <viacheslav.kravtsov@gmail.com>
2025-05-18 19:55:18 +0300

    Update accounts.py
    Ver.2 список объектов User внутри класса Admin. Исправлена ошибка с нумерацией идентификатора user

--- accounts.py
+++ accounts.py
@@ -8,38 +8,58 @@
 # Класс должен также содержать методы add_user и remove_user, которые позволяют добавлять и удалять пользователей из списка (представь, что это просто список экземпляров User).
 
 class User():
-    def __init__(self, user_id, user_name):
-        self.user_id = user_id
+    # конструктор, по умолчанию уровень доступа "user"
+    def __init__(self, user_idf, user_name):
+        self.user_idf = user_idf
         self.user_name = user_name
-        self.access_level = ["user"]
+        self.__access_level = "user"
+
+    def get_access_level(self):
+        return self.__access_level
+
+    def set_access_level(self, new_level):
+        self.__access_level = new_level
 
 class Admin(User):
-    def __init__(self, admin_name):
-        new_id = len(user_list) + 1
-        super().__init__(new_id, admin_name)
-        self.access_level.append("admin")
-        user_list.append(self)
-        print(f"Администратор {self.user_name} с ID {self.user_id} создан, уровен доступа {self.access_level}")
+    def __init__(self, name):
+        self.id_count = 1
+        self.user_list = []
+        super().__init__(self.id_count, name)
+        self.id_count += 1
+        self.set_access_level("admin")
+        print(f"Администратор {self.user_name} с ID {self.user_idf} создан, уровень доступа {self.get_access_level()}")
 
     def add_user(self, new_name):
-        new_id = len(user_list) + 1
-        new_user = User(new_id, new_name)
-        user_list.append(new_user)
-        print(f"Пользователь {new_user.user_name} с ID {new_user.user_id} добавлен, уровен доступа {new_user.access_level}")
+        new_user = User(self.id_count, new_name)
+        self.id_count += 1
+        self.user_list.append(new_user)
+        print(f"Пользователь {new_user.user_name} с ID {new_user.user_idf} добавлен, уровень доступа {new_user.get_access_level()}")
 
-    def remove_user(self, user_id):
-        if user_id < 1 or user_id > len(user_list):
-            print(f"Пользователь с ID {user_id} не найден")
-        else:
-            user = user_list.pop(user_id - 1)
-            print(f"Пользователь {user.user_name} с ID {user.user_id} удален")
+    def remove_user(self, user_idf):
+        ok_remove = False
+        for i in range(0, len(self.user_list)):
+            if self.user_list[i].user_idf == user_idf:
+                user = self.user_list.pop(i)
+                print(f"Пользователь {user.user_name} с ID {user.user_idf} удален")
+                ok_remove = True
+                break
+        if not ok_remove:
+            print(f"Пользователь с ID {user_idf} не найден")
 
-    def user_info(self):
+    def users_list(self):
         print("Список всех пользователей")
-        for user in user_list:
-            print(f"ID: {user.user_id}, имя: {user.user_name}, уровень доступа: {user.access_level}")
+        for user in self.user_list:
+            print(f"ID: {user.user_idf}, имя: {user.user_name}, уровень доступа: {user.get_access_level()}")
 
-user_list = []
+    def get_user_idf(self, user_name):
+        user_idf = 0
+        for user in self.user_list:
+            if user.user_name == user_name:
+                user_idf = user.user_idf
+        if user_idf == 0:
+            print(f"Пользователь с именем {user_name} не найден")
+        return user_idf
+
 
 admin1 = Admin("Александр")
 
@@ -47,9 +67,13 @@ admin1.add_user("Сергей")
 admin1.add_user("Владимир")
 admin1.add_user("Николай")
 
-admin1.user_info()
+admin1.users_list()
 
-admin1.remove_user(3)
+user_id = admin1.get_user_idf("Григорий")
+admin1.remove_user(user_id)
 
-admin1.user_info()
+user_id = admin1.get_user_idf("Николай")
+admin1.remove_user(user_id)
+
+admin1.users_list()
 
