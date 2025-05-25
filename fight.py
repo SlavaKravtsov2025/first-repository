54013e9aba90e2881e16bd2048ac8a6452155578
SlavaKravtsov2025 <viacheslav.kravtsov@gmail.com>
2025-05-25 19:28:27 +0300

    Update fight.py
    Добавил оружие.
    Обновил сценарий.
    Добавил 3 попытки.
    Фин. версия.

--- fight.py
+++ fight.py
@@ -8,8 +8,8 @@
 from abc import ABC, abstractmethod
 
 class Fighter():
-    def __init__(self):
-        self.weapon = None
+    def __init__(self, new_weapon = None):
+        self.weapon = new_weapon
 
     def change_weapon(self, new_weapon):
         self.weapon = new_weapon
@@ -32,23 +32,39 @@ class Sword(Weapon):
     def __init__(self):
         self.name = "меч"
     def attack(self):
-        print("Я атакую мечом")
+        print("Ты атакуешь мечом")
 
 class Bow(Weapon):
     def __init__(self):
         self.name = "лук"
     def attack(self):
-        print("Я стреляю из лука")
+        print("Ты стреляешь из лука")
 
 class Spear(Weapon):
     def __init__(self):
         self.name = "копье"
     def attack(self):
-        print("Я атакую копьем")
+        print("Ты атакуешь копьем")
 
-print("Борьба с монстрами (вер.0.1)")
+class Sling(Weapon):
+    def __init__(self):
+        self.name = "праща"
+    def attack(self):
+        print("Ты атакуешь из пращи")
+
+class Baton(Weapon):
+    def __init__(self):
+        self.name = "дубинка"
+    def attack(self):
+        print("Ты атакуешь дубинкой")
+
+print("Борьба с монстрами (вер.1.0)")
 
 fighter = Fighter()
+
+weapon_list = [Sword(), Bow(), Spear(), Sling(), Baton()]
+
+weapon_cnt = len(weapon_list)
 
 while True:
     monster_type = input("Выбери монстра (1 - Дракон, 2 - Вампир, 3 - Зомби, 0 - Выход): ")
@@ -59,29 +75,33 @@ while True:
     elif monster_type == "2":
         monster = Monster("Вампир", "копье")
     elif monster_type == "3":
-        monster = Monster("Зомби", "меч")
+        monster = Monster("Зомби", "дубинка")
     else:
         print(f"Неправильный тип монстра {monster_type}")
         continue
 
+    nn = 0
     while True:
-        weapon_type = input("Выбери оружие (1 - Меч, 2 - Лук, 3 - Копье, 0 - Выбор монстра): ")
-        if weapon_type == "0":
+        weapon_type = input(f"Выбери правильное оружие - 3 попытки (цифры от 1 до {weapon_cnt}, 0-Возврат к выбору монстра): ")
+        wt = int(weapon_type)
+        if wt == 0:
             break
-        elif weapon_type == "1":
-            weapon = Sword()
-        elif weapon_type == "2":
-            weapon = Bow()
-        elif weapon_type == "3":
-            weapon = Spear()
+        elif 1 <= wt <= weapon_cnt:
+            weapon = weapon_list[wt-1]
         else:
-            print(f"Неправильный тип оружия {monster_type}")
+            print(f"Неправильный тип оружия {weapon_type}")
             continue
 
         fighter.change_weapon(weapon)
         fighter.weapon.attack()
         if weapon.name == monster.get_weapon_kill():
-            print(f"Монстр {monster.name} убит")
+            print(f"Монстр {monster.name} побежден")
             break
+        else:
+            print(f"Монстр {monster.name} отбил атаку")
 
+        nn += 1
+        if nn >= 3:
+            print("Вы проиграли, 3 попытки исчерпано, монстр убил Вас")
+            break
 
